643e8bff68b5f27377f05ae982074a6ad962f106
mohammadser <johndoe@example.com>
2022-06-19 00:47:18 +1000

    commit first step

---
+++ platform_info.py
@@ -0,0 +1,8 @@
+import platform
+
+print(platform.platform())
+print(platform.system())
+print(platform.release())
+print(platform.version())
+print(platform.version().split('.')[2]) 
+print(platform.machine())
