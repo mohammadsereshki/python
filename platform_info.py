8af7399b40591517c8ceeefc0d9708172496b2d0
mohammadser <johndoe@example.com>
2022-06-20 19:16:50 +1000

    test1

--- platform_info.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-print(platform.platform())
-print(platform.system())
-print(platform.release())
-print(platform.version())
-print(platform.version().split('.')[2]) 
-print(platform.machine())
